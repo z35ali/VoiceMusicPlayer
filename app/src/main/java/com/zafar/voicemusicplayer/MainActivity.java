053aff0d678c4a2a73672171e2ae4bd41811afee
Zafar <zafar.a.ali2@gmail.com>
2019-04-28 13:31:54 -0400

    Moved displaySongNames method location

--- app/src/main/java/com/zafar/voicemusicplayer/MainActivity.java
+++ app/src/main/java/com/zafar/voicemusicplayer/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         refresh = findViewById(R.id.refresh_btn);
         handler = new Handler();
         appExternalStoragePermission();
+        displaySongNames();
 
        animation = new RotateAnimation(0.0f, 360.0f,
                 Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
@@ -62,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displaySongNames();
+
                 ((ImageView)findViewById(R.id.refresh_btn)).setAnimation(animation);
                 runnable = new Runnable() {
                     @Override
